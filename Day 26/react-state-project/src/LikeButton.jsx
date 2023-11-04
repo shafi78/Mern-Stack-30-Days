743ea40d770cda542894127a852c1987b1fea51b
shafi <shafiullaattar786@gmail.com>
2023-11-05 00:24:18 +0530

    Day 26 third commit

--- Day 26/react-state-project/src/LikeButton.jsx
+++ Day 26/react-state-project/src/LikeButton.jsx
@@ -3,19 +3,31 @@ import React, { useState } from 'react'
 const LikeButton = () => {
 
     const [islike,setIsLike] = useState(false);
+    const [count,setCount] = useState(0);
 
     function clicked() {
         let newVal = !islike ;
         setIsLike(newVal);
     }
+
+    function btnClick() {
+      setCount((prev) => {
+        return prev+1 ;
+      })
+
+      setCount((prev) => {
+       return prev+1 ;
+      })
+    }
     
   return (
     <div>
-        <p onDoubleClick={clicked}>
+        <p onClick={clicked}>
             {islike ? (<i className='fa-regular fa-heart'></i>) :
-            (<i className='fa-solid fa-heart' style={{color:"red",fontSize:"2rem"}}></i>)
+            (<i className='fa-solid fa-heart' style={{color:"red"}}></i>)
             }
         </p>
+        <h3 onClick={btnClick} style={{cursor:"pointer"}}>{count}</h3>
     </div>
   )
 }
